e1fd5189c7a10445928a0fec574fcd027e6b629a
rrevein946 <rrevein946@gmail.com>
2026-06-05 05:09:02 +0300

    upd to keep up w everything else

--- public/logout.php
+++ public/logout.php
@@ -1,13 +1,10 @@
 <?php
-// Простой и надёжный выход
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Очищаем все данные сессии
 $_SESSION = [];
 
-// Удаляем куки сессии, если есть
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -16,9 +13,7 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Уничтожаем сессию
 session_destroy();
 
-// Редирект на главную
 header('Location: /');
 exit;
